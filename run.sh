1623092380cc5a9c1aadb893cbc39570c89db4ee
jankovidakovic <janko.vidakovic@fer.hr>
2022-05-30 19:48:16 +0000

    Add run script

---
+++ run.sh
@@ -0,0 +1,8 @@
+python run.py \
+	--config experiments/adapters/test/config.yaml \
+	--device cuda \
+	--model_name distilbert-base-uncased \
+	--run_name test_before_final \
+	--run_dir runs/adapters/test_before_final \
+	--n_runs 2
+
