50299b78952d9bccce244ab283b621f4fd62ca56
jankovidakovic <janko.vidakovic@fer.hr>
2022-06-02 23:30:58 +0200

    Add inference

--- experiments/model.py
+++ experiments/model.py
@@ -1,24 +1,37 @@
+import os
+
+import torch
 from torch.optim import AdamW
-from transformers import AutoAdapterModel, PfeifferConfig, get_scheduler, AdapterConfig
+from transformers import AutoAdapterModel, get_scheduler, AdapterConfig
 
 
-def get_model(model_name: str, model_config: dict):
-    model = AutoAdapterModel.from_pretrained(model_name)
+def get_model(model_name_or_path: str, model_config: dict, train: bool = True, load_state_dict: bool = True):
+    model = AutoAdapterModel.from_pretrained(model_name_or_path)
     # add classification head (regardless of the model)
-    model.add_classification_head("emo", num_labels=4)
+    if not load_state_dict:
+        model.add_classification_head("emo", num_labels=4)
 
     if model_config["type"] == "adapter":
         adapter_config = AdapterConfig.load(model_config["adapter_config"])
+        if load_state_dict:
+            model.delete_adapter("emo")  # delete the previous notion of adapter
         model.add_adapter("emo", adapter_config)
-        model.train_adapter("emo")
         model.set_active_adapters(["emo"])
-    elif model_config["type"] == "bitfit":
+        if train:
+            model.train_adapter("emo")
+    elif model_config["type"] == "bitfit" and not train:
         # freeze all model parameters except the biases and the heads
         for name, param in model.named_parameters():
             if 'bias' not in name and 'head' not in name:
                 param.requires_grad = False
     else:
         raise ValueError(f"Unknown model type: {model_config['type']}")
+
+    if load_state_dict:
+        model.load_state_dict(torch.load(os.path.join(model_name_or_path, "pytorch_model.bin")))
+
+    if not train:
+        model.eval()
 
     return model
 
