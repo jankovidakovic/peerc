b179de50460c31f6d30afcfbefc2cbf062c98522
jankovidakovic <janko.vidakovic@fer.hr>
2022-06-04 10:59:20 +0000

    Modify run bitfit script

--- run-bitfit.sh
+++ run-bitfit.sh
@@ -3,7 +3,7 @@ WANDB_LOG_MODEL=true python -m experiments.main \
 	--config config/bitfit.yaml \
 	--device cuda \
 	--model_name "roberta-base" \
-	--run_name "bitfit-final" \
-	--run_dir "runs/bitfit/bitfit-final" \
+	--run_name "bitfit-lr-sched" \
+	--run_dir "runs/bitfit/bitfit-lr-sched" \
 	--n_runs 10
 
